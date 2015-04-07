9df5131f742d0018d82d4b602b6a623f14ff69e2
Rahul Horé <hore.rahul@gmail.com>
2015-04-06 20:27:49 -0400

    Fixes kadane-test

--- test/algorjthms/core_test.clj
+++ test/algorjthms/core_test.clj
@@ -5,5 +5,5 @@
 (deftest kadane-test
   (testing "kadane"
     (is (= 10 (kadane [2 -1 2 3 4 -5])))
-    (is (= -1 (kadane [-3 -4 -1 -2])))))
-    (is (=  0 (kadane [])))
+    (is (= -1 (kadane [-3 -4 -1 -2]))))
+    (is (=  0 (kadane []))))
