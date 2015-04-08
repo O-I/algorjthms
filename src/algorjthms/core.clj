14fe05cdaa52ff3724e584537da589dd43754d54
Rahul Horé <hore.rahul@gmail.com>
2015-04-07 22:22:33 -0400

    Adds lazy quicksort

--- src/algorjthms/core.clj
+++ src/algorjthms/core.clj
@@ -1,5 +1,7 @@
 (ns algorjthms.core)
 
+; Slight modification of Jean Niklas L'Orange's implementation
+; https://groups.google.com/forum/#!topic/clojure/J-L3BUTaJ3E/discussion
 (defn kadane
   "Returns the maximum sum of a contiguous subarray of a vector.
    If the vector consists of all negative numbers, the minimum
@@ -8,3 +10,23 @@
   (let [pos+ (fn [sum x] (if (neg? sum) x (+ sum x)))
         partial-sums (reductions pos+ (or head 0) tail)]
     (reduce max partial-sums)))
+
+; from The Joy of Clojure
+(defn- sort-parts
+  "Lazy, tail-recursive, incremental quicksort. Works against
+   and creates partitions based on the pivot, defined as 'work'."
+  [work]
+  (lazy-seq
+    (loop [[part & parts] work]
+      (if-let [[pivot & xs] (seq part)]
+        (let [smaller? #(< % pivot)]
+          (recur (list*
+                   (filter smaller? xs)
+                   pivot
+                   (remove smaller? xs)
+                   parts)))
+        (when-let [[x & parts] parts]
+          (cons x (sort-parts parts)))))))
+
+(defn quicksort [xs]
+  (sort-parts (list xs)))
