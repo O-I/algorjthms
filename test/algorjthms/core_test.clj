e6f6d9b6ff897ed3b51f0d18b3b6383a5b3c0747
Rahul Horé <hore.rahul@gmail.com>
2015-04-10 21:44:30 -0400

    Adds quicksort-test

--- test/algorjthms/core_test.clj
+++ test/algorjthms/core_test.clj
@@ -7,3 +7,7 @@
     (is (= 10 (kadane [2 -1 2 3 4 -5])))
     (is (= -1 (kadane [-3 -4 -1 -2]))))
     (is (=  0 (kadane []))))
+
+(deftest quicksort-test
+  (testing "quicksort"
+    (is (= (range 100) (-> 100 range shuffle quicksort)))))
