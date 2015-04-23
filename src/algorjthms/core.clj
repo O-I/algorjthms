aace2fc8c33128412d86377337ec0e7477e14dc0
Rahul Horé <hore.rahul@gmail.com>
2015-04-22 20:54:39 -0400

    Adds Damerau-Levenshtein

--- src/algorjthms/core.clj
+++ src/algorjthms/core.clj
@@ -51,3 +51,38 @@
    [seq1 seq2]
    (when (= (count seq1) (count seq2))
      (count (remove (fn [[x y]] (= x y)) (partition 2 (interleave seq1 seq2))))))
+
+; from the Leiningen source code
+(defn- next-dist-row [s t x pprev prev]
+  (let [t-len (count t)
+        eq-chars (fn [x y] (= (nth s x) (nth t (dec y))))]
+    (reduce (fn [row y]
+              (let [min-step
+                    (cond->
+                     (min (inc (peek row)) ; addition cost
+                          (inc (get prev y)) ; deletion cost
+                          (cond-> (get prev (dec y)) ; substitution cost
+                                  (not (eq-chars x y)) inc))
+                     (and (pos? x) (pos? (dec y)) ; check for transposition
+                          (eq-chars x (dec y))
+                          (eq-chars (dec x) y)
+                          (not (eq-chars x y)))
+                     (min (inc (get pprev (- y 2)))))] ; transposition cost
+                (conj row min-step)))
+            [(inc x)]
+            (range 1 (inc t-len)))))
+
+(defn damerau–levenshtein
+  "Returns the Damerau–Levenshtein distance between two strings."
+  [s t]
+  (let [s-len (count s)
+        t-len (count t)
+        first-row (vec (range (inc t-len)))
+        matrix (reduce (fn [matrix x]
+                         (conj matrix
+                               (next-dist-row s t x
+                                              (peek (pop matrix))
+                                              (peek matrix))))
+                       [[] first-row]
+                       (range s-len))]
+    (peek (peek matrix))))
