59817dc8c1653b6f7bcac770bd937a577d5df5cb
Rahul Horé <hore.rahul@gmail.com>
2015-04-29 23:26:17 -0400

    Adds merge-sort

--- src/algorjthms/core.clj
+++ src/algorjthms/core.clj
@@ -116,3 +116,18 @@
   "Returns the greatest common divisor of two integers."
   [a b]
   (if (zero? b) a (recur b (mod a b))))
+
+(defn- merge* [left right]
+  (cond (nil? left)  right
+        (nil? right) left
+        :else (let [[l & *left]  left
+                    [r & *right] right]
+               (if (<= l r) (cons l (merge* *left right))
+                            (cons r (merge* left *right))))))
+
+(defn merge-sort [L]
+  (let [[l & *L] L]
+    (if (nil? *L)
+      L
+      (let [[left right] (split-at (/ (count L) 2) L)]
+        (merge* (merge-sort left) (merge-sort right))))))
