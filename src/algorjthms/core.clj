ad407ba260b1c36c4be77ed7b8185de5d0d97ed7
Rahul Horé <hore.rahul@gmail.com>
2015-05-05 23:37:49 -0400

    Adds modular exponentiation

--- src/algorjthms/core.clj
+++ src/algorjthms/core.clj
@@ -154,3 +154,12 @@
   "Calculates the Manhattan distance between two points"
   [[x1 y1] [x2 y2]]
   (+ (Math/abs ^Integer (- x2 x1)) (Math/abs ^Integer (- y2 y1))))
+
+(defn power-mod
+  "Calculates modular exponentiation b^e mod m"
+  [b e m]
+  (defn m* [p q] (mod (* p q) m))
+  (loop [b b e e x 1]
+    (if (zero? e) x
+      (if (even? e) (recur (m* b b) (/ e 2) x)
+        (recur (m* b b) (quot e 2) (m* b x))))))
