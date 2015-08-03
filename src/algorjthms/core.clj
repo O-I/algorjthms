00495b4ecd5a942bb6fad1c69af933e3ef60d873
Rahul Horé <hore.rahul@gmail.com>
2015-08-02 23:01:51 -0400

    Refactors luhn?

--- src/algorjthms/core.clj
+++ src/algorjthms/core.clj
@@ -463,8 +463,8 @@
   (lazy-cat [0 1] (map +' fibonacci (rest fibonacci))))
 
 (defn luhn? [cc]
-  (let [factors (flatten (repeat [1 2]))
-        numbers (map #(Character/digit % 10) (seq cc))
-        sum (reduce + (map #(int (+ (/ %1 10) (mod %1 10)))
-                        (map * (reverse numbers) factors)))]
-    (zero? (mod sum 10))))
+  (let [factors (cycle [1 2])
+        digits (map #(Character/digit % 10) (seq cc))
+        sum (reduce + (map #(+ (quot % 10) (rem % 10))
+                      (map * (reverse digits) factors)))]
+    (zero? (rem sum 10))))
