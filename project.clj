3c83f3912e97e9c3f0cada07a004ddebab8d43d0
Rahul Horé <hore.rahul@gmail.com>
2015-09-25 23:46:45 -0400

    Updates project.clj

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/O-I/algorjthms"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0-alpha5"]
                  [org.clojure/math.numeric-tower "0.0.4"]])
